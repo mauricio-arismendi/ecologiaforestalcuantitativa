f9711a68ca29d6f6e2f2d22a1e914579d48186b8
Mauricio Arismendi <mauricio.arismendi.aedo@gmail.com>
2025-04-20 22:49:49 -0300

    Add files via upload

--- tareitas.R
+++ tareitas.R
@@ -46,3 +46,36 @@ mean(df$atot)
 ## 3. compare los modelos, basado en los estadisticos de
 ##    validacion calculados
 ## flojera, pero se observa en la gráfica que el modelo 3 es mejor. 
+
+
+
+##altura4.r
+##Tarea sugerida:
+## 1. realice un grafico entre el error de prediccion y el valor observado
+plot(e.aju~atot, data=df, las=1, col="gray")
+## 2. realice un grafico entre el error de prediccion y el valor ajustado
+plot(e.aju~aju, data=df, las=1, col="blue")
+## 3. realice un grafico entre el error de prediccion y el diametro
+plot(e.aju~dap, data=df, las=1, col="red")
+
+##altura5.r
+
+##Tarea sugerida:
+## 1. prepare un cuadro en una hoja a mano, y escriba los estadisticos de
+##    validacion para cada modelo (cada fila un modelo).
+
+100*ad.mod1/mean.h
+100*rmsd.mod1/mean.h
+100*aad.mod1/mean.h
+##modelo 2
+100*ad.mod2/mean.h
+100*rmsd.mod2/mean.h
+100*aad.mod2/mean.h
+
+
+
+## 3. compare ambos modelos, basado en los estadisticos de
+##    validacion calculados.
+#Mod 1 es mejor 
+
+
